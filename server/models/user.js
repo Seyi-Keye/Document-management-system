7c502b547ca64e0340e06c71196bb96896f50d53
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2017-03-11 23:32:13 +0100

    models restructure

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,3 @@
-const bcrypt = require('bcrypt');
-
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
@@ -53,48 +51,6 @@ module.exports = (sequelize, DataTypes) => {
         });
       }
     },
-
-    instanceMethods: {
-        confirmPassword() {
-          if (this.password !== this.passwordConfirmation) {
-            throw new Error('Password does not match');
-          }
-          return true;
-        },
-
-      /**
-       * Compare plain password to user's hashed password
-       * @method
-       * @param {String} password
-       * @returns {Boolean} password match
-       */
-        validPassword(password) {
-        return bcrypt.compareSync(password, this.password);
-        },
-
-      /**
-       * Hash user's password
-       * @method
-       * @returns {void} no return
-       */
-        hashPassword() {
-        this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
-        }
-    },
-
-    hooks: {
-      beforeCreate(user) {
-        if (user.confirmPassword()) {
-          user.hashPassword();
-        }
-      },
-
-      beforeUpdate(user) {
-        if (user._changed.password) {
-          user.hashPassword();
-        }
-      }
-    }
   });
   return User;
 };
