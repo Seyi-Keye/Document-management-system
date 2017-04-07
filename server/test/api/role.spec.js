dfa1457abe4ed01d201452164d099406c89d2019
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2017-04-07 16:59:40 +0100

    role api test refactored

--- server/test/api/role.spec.js
+++ server/test/api/role.spec.js
@@ -2,63 +2,115 @@ import supertest from 'supertest';
 import chai from 'chai';
 import app from '../../../server';
 import specHelpers from '../helpers/specHelpers';
-import { Document, Role, User } from '../../models';
+import models from '../../models';
 import SeedHelper from '../helpers/seedHelper';
 
-const request = supertest.agent(app);
+const server = supertest.agent(app);
 const expect = chai.expect;
 const adminUser = specHelpers.adminUser;
-let token;
-let regularRole;
 
 describe('Role Api:', () => {
-  before((done) => {
-    SeedHelper
-      .init()
-      .then(() => {
-        Role
-          .findOne({
-            where: {
-              title: 'admin'
-            }
-          })
-          .then((foundAdmin) => {
-            adminUser.RoleId = foundAdmin.dataValues.id;
-            User
-              .create(adminUser)
-              .then(() => {
-                request
-                  .post('/api/v1/users/login')
-                  .send(adminUser)
-                  .end((err, res) => {
-                    if (err) {
-                      return err;
-                    }
-                    token = res.body.token;
-                    done();
-                  });
-              });
-          });
+  let token, regularRole;
+
+  const promisify = (path, data) => new Promise((resolve, reject) => {
+    server
+      .post(path)
+      .set('Content-Type', 'application/json')
+      .send(data)
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
       });
   });
 
-  after((done) => {
-    User.destroy({ where: {} });
-    Document.destroy({ where: {} });
-    done();
-  });
-
-  Role
+  before((done) => {
+    SeedHelper
+      .init()
+      .then(() => models.Role
+      .findOne({
+        where: {
+          title: 'admin'
+        }
+      }))
+  .then((foundAdmin) => {
+    adminUser.RoleId = foundAdmin.dataValues.id;
+    return models.User.create(adminUser);
+  })
+  .then(() => promisify('/api/v1/users/login', adminUser))
+  .then((res) => {
+    token = res.body.token;
+    return models.Role
     .findOne({
       where: {
         title: 'regular'
       }
-    })
-    .then((regular) => {
-      regularRole = regular;
     });
-  it('Finds role: fails to get all roles without authorized token', (done) => {
-    request
+  })
+  .then((regular) => {
+    regularRole = regular;
+    done();
+  });
+  });
+
+  after((done) => {
+    models
+      .sequelize
+      .sync({ force: true });
+    done();
+  });
+
+  describe('Create Role', () => {
+    it('fails to create new role on null title', () => {
+      server
+      .post('/api/v1/roles')
+      .set({ Authorization: token })
+      .send({})
+      .end((err, response) => {
+        if (err) {
+          return err;
+        }
+        expect(response.status)
+          .to
+          .equal(400);
+      });
+    });
+
+    it('fails to create duplicate role', () => {
+      server
+      .post('/api/v1/roles')
+      .set({ Authorization: token })
+      .send({ title: 'admin' })
+      .end((err, response) => {
+        if (err) {
+          return err;
+        }
+        expect(response.status)
+          .to
+          .equal(400);
+      });
+    });
+
+    it('fails for invalid attributes', () => {
+      server
+      .post('/api/v1/roles')
+      .send({ name: 'hello' })
+      .set({ Authorization: token })
+      .end((err, response) => {
+        if (err) {
+          return err;
+        }
+        expect(response.status)
+          .to
+          .equal(400);
+      });
+    });
+  });
+
+  describe('Get Roles: ', () => {
+    it('Finds role: fails to get all roles without authorized token', (done) => {
+      server
       .get('/api/v1/roles')
       .end((err, res) => {
         if (err) {
@@ -72,10 +124,10 @@ describe('Role Api:', () => {
           .equal('Token required for access');
         done();
       });
-  });
+    });
 
-  it('Finds role: returns all roles to an admin user', (done) => {
-    request
+    it('Finds role: returns all roles to an admin user', (done) => {
+      server
       .get('/api/v1/roles')
       .set({ Authorization: token })
       .end((err, res) => {
@@ -87,55 +139,10 @@ describe('Role Api:', () => {
           .equal(200);
         done();
       });
-  });
+    });
 
-  it('Create role: fails to create new role on null title', () => {
-    request
-      .post('/api/v1/roles')
-      .set({ Authorization: token })
-      .send({})
-      .end((err, response) => {
-        if (err) {
-          return err;
-        }
-        expect(response.status)
-          .to
-          .equal(400);
-      });
-  });
-
-  it('Create role: fails to create duplicate role', () => {
-    request
-      .post('/api/v1/roles')
-      .set({ Authorization: token })
-      .send({ title: 'admin' })
-      .end((err, response) => {
-        if (err) {
-          return err;
-        }
-        expect(response.status)
-          .to
-          .equal(400);
-      });
-  });
-
-  it('Create role: fails for invalid attributes', () => {
-    request
-      .post('/api/v1/roles')
-      .send({ name: 'hello' })
-      .set({ Authorization: token })
-      .end((err, response) => {
-        if (err) {
-          return err;
-        }
-        expect(response.status)
-          .to
-          .equal(400);
-      });
-  });
-
-  it('Find role: returns correct role when a valid id is passed', (done) => {
-    request
+    it('Find role: returns correct role when a valid id is passed', (done) => {
+      server
       .get(`/api/v1/roles/${regularRole.dataValues.id}`)
       .set({ Authorization: token })
       .end((err, response) => {
@@ -147,10 +154,12 @@ describe('Role Api:', () => {
           .equal(200);
         done();
       });
+    });
   });
 
-  it('Update role: should update an existing role', (done) => {
-    request
+  describe('Update and delete Role: ', () => {
+    it('Update role: should update an existing role', (done) => {
+      server
       .put(`/api/v1/roles/${regularRole.dataValues.id}`)
       .send({ title: 'role' })
       .set({ Authorization: token })
@@ -163,10 +172,10 @@ describe('Role Api:', () => {
           .equal(200);
         done();
       });
-  });
+    });
 
-  it('Delete role: deletes a role', (done) => {
-    request
+    it('Delete role: deletes a role', (done) => {
+      server
       .delete(`/api/v1/roles/${regularRole.dataValues.id}`)
       .set({ Authorization: token })
       .end((err, response) => {
@@ -178,5 +187,6 @@ describe('Role Api:', () => {
           .equal(200);
         done();
       });
+    });
   });
 });
